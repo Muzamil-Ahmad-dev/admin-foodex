43f70a2006130ab707f93b4779e2d6a7183510d6
Muzamil Ahmad <muzamilsakhi079@gmail.com>
2026-02-12 14:05:22 +0500

    add utills for token saving

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import store from "../app/store"; // make sure the path matches your project
+
+const BASE_URL = "https://foodex-backend--muzamilsakhi079.replit.app/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // always send cookies (refresh token)
+});
+
+// 🔥 Axios interceptor automatically attaches accessToken
+api.interceptors.request.use((config) => {
+  const { user, accessToken } = store.getState().auth;
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+export default api;
